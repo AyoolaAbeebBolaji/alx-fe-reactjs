d1208363456dd5b5e3d01a50af7d847e3c074e28
Ayoola <ayoolaabeebbolaji@gmail.com>
2025-11-09 10:21:19 +0100

    Refactor Navbar: remove inline styles, add Navbar.css

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+/* import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
@@ -15,4 +15,31 @@ export default function Navbar() {
       <Link to="/contact">Contact</Link>
     </nav>
   );
+} */
+
+import React from "react";
+import "./Navbar.css";
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="navbar-logo">MyLogo</div>
+      <ul className="navbar-links">
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/about">About</a>
+        </li>
+        <li>
+          <a href="/services">Services</a>
+        </li>
+        <li>
+          <a href="/contact">Contact</a>
+        </li>
+      </ul>
+    </nav>
+  );
 }
+
+export default Navbar;
